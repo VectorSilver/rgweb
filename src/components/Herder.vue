6779aa52640e61471f14641c5fa7c63e90767a35
huangxuyi123 <408625144@qq.com>
2019-07-04 13:05:02 +0800

    1

--- src/components/Herder.vue
+++ src/components/Herder.vue
@@ -1,19 +1,5 @@
 <template>
-  <div>
-    <div id="header">
-      <a>您好，请登录</a>
-      <a>注册</a>
-      <a>购物车(0)</a>
-      <a>我的订单</a>
-      <a>我的睿购</a>
-      <a>关于睿购商城</a>
-    </div>
-
-
-
-
-
-  </div>
+  
 </template>
 
 <script>
