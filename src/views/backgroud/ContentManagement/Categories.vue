591c7d7e1fd27480edfa1189f3667f274073bc0a
huangxuyi123 <408625144@qq.com>
2019-07-15 17:25:06 +0800

    1

--- src/views/backgroud/ContentManagement/Categories.vue
+++ src/views/backgroud/ContentManagement/Categories.vue
@@ -1,60 +1,108 @@
 <template>
-    <div>
-        
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+ <div class="custom-tree-container">
+  <div class="block">
+    <p>内容分类管理</p>
+    <el-tree
+      :data="data"
+      show-checkbox
+      node-key="id"
+      default-expand-all
+      :expand-on-click-node="false"
+      :render-content="renderContent">
+    </el-tree>
+  </div>
+</div>
 
-    </div>
 </template>
 
 <script>
+  let id = 1000;
+
   export default {
     data() {
+      const data = [{
+        id: 1,
+        label: '睿购商城',
+        children: [{
+          id: 4,
+          label: '首页',
+          children: [{
+            id: 9,
+            label: '大广告'
+          }, {
+            id: 10,
+            label: '中广告'
+          }, {
+            id: 11,
+            label: '小广告'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: '列表页面',
+        children: [{
+          id: 5,
+          label: '二级 2-1'
+        }, {
+          id: 6,
+          label: '二级 2-2'
+        }]
+      }, {
+        id: 3,
+        label: '详细页面',
+        children: [{
+          id: 7,
+          label: '二级 3-1'
+        }, {
+          id: 8,
+          label: '二级 3-2'
+        }]
+      }];
       return {
-        data: [{
-          label: '一级 1',
-          children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
-          }]
-        }, {
-          label: '一级 2',
-          children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
-          }]
-        }, {
-          label: '一级 3',
-          children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
-      };
+        data: JSON.parse(JSON.stringify(data)),
+        data: JSON.parse(JSON.stringify(data))
+      }
     },
+
     methods: {
-      handleNodeClick(data) {
-        console.log(data);
+      append(data) {
+        const newChild = { id: id++, label: 'New test', children: [] };
+        if (!data.children) {
+          this.$set(data, 'children', []);
+        }
+        data.children.push(newChild);
+      },
+        
+
+      remove(node, data) {
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === data.id);
+        children.splice(index, 1);
+      },
+
+      renderContent(h, { node, data, store }) {
+        return (
+          <span class="custom-tree-node">
+            <span>{node.label}</span>
+            <span>
+              <el-button size="mini" type="text" on-click={ () => this.append(data) }>新增</el-button>
+              <el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>删除</el-button>
+              <el-button size="mini" type="text" >修改</el-button>
+            </span>
+          </span>);
       }
     }
   };
 </script>
+
+<style>
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
+</style>
