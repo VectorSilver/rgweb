e779cd8b8c006c067b7cfe37e9ddb4db8bcca8e7
RejoiceW <956882148@qq.com>
2019-07-19 16:32:45 +0800

    1

---
+++ src/views/backgroud/InforSta/TongjiNumber.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>信息统计</el-breadcrumb-item>
+      <el-breadcrumb-item>统计数量</el-breadcrumb-item>
+    </el-breadcrumb>
+    <br />
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
       //     '^/api': ''
       //   }
       '/api': { //奕桦
-        target: 'http://10.25.193.31:9999', //源地址
+        target: 'http://10.25.193.205:9999', //源地址
         changeOrigin: true, //允许跨域
         ws: true, //是否代理websockets
         pathRewrite: {
