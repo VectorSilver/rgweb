eb0d5ae8efa00e69f7e19f0c51a7fbc672b79e8e
qigreen <1399219384@qq.com>
2019-07-19 16:30:43 +0800

    1234

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     devServer: {
       proxy: {
-          '/api': { //奕桦
+          '/api': { //qinzi
             target: 'http://10.25.193.146:9999', //源地址
             changeOrigin: true, //允许跨域
             ws: true, //是否代理websockets
